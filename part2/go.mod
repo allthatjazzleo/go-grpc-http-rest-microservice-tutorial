e96f831c8f3520e32efc414efa74af438b5a6781
Leo Pang <leo.pang@plutux.com>
2019-06-11 17:31:25 +0800

    finish part2

--- part2/go.mod
+++ part2/go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/golang/protobuf v1.3.1
 	github.com/google/btree v1.0.0 // indirect
 	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.9.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.0
 	github.com/kr/pty v1.1.4 // indirect
 	github.com/rogpeppe/fastuuid v1.1.0 // indirect
 	go.opencensus.io v0.22.0 // indirect
@@ -23,7 +23,7 @@ require (
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
 	golang.org/x/tools v0.0.0-20190608022120-eacb66d2a7c3 // indirect
 	google.golang.org/appengine v1.6.1 // indirect
-	google.golang.org/genproto v0.0.0-20190605220351-eb0b1bdb6ae6 // indirect
+	google.golang.org/genproto v0.0.0-20190605220351-eb0b1bdb6ae6
 	google.golang.org/grpc v1.21.1
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 	honnef.co/go/tools v0.0.0-20190607181801-497c8f037f5a // indirect
--- part2/test.go
+++ part2/test.go
@@ -2,26 +2,29 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-type A struct {
-	b string
+func doubleSlice(s interface{}) interface{} {
+	if reflect.TypeOf(s).Kind() != reflect.Slice {
+		fmt.Println("The interface is not a slice.")
+		return nil
+	}
+
+	v := reflect.ValueOf(s)
+	newLen := v.Len()
+	newCap := (v.Cap() + 1) * 2
+	typ := reflect.TypeOf(s).Elem()
+
+	t := reflect.MakeSlice(reflect.SliceOf(typ), newLen, newCap)
+	reflect.Copy(t, v)
+	return t.Interface()
 }
 
 func main() {
-	a := []*A{}
-	b := new(A)
-	b.b = "sdas"
-	a = append(a, b)
-	c(a)
-	drivers := make(map[string]string)
-	drivers["driverName"] = "sads"
-	driveri := drivers["driverName"]
-	fmt.Println("dr", driveri)
-	// fmt.Println("ok", ok)
-}
+	xs := doubleSlice([]string{"foo", "bar"}).([]string)
+	fmt.Println("data =", xs, "len =", len(xs), "cap =", cap(xs))
 
-func c(a []*A) {
-	fmt.Println(a)
-	fmt.Println(a[0].b)
+	ys := doubleSlice([]int{3, 1, 4}).([]int)
+	fmt.Println("data =", ys, "len =", len(ys), "cap =", cap(ys))
 }
