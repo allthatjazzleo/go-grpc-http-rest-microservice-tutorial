ac9ab7b03323c088c5122b3c5da091c0147b4ad0
Leo Pang <leo.pang@plutux.com>
2019-06-11 17:36:01 +0800

    complete sh part2

--- part2/cmd/server/init.sh
+++ part2/cmd/server/init.sh
@@ -1,2 +1,2 @@
 #! bin/sh
-./server -grpc-port=9090 -db-host=localhost:3306 -db-user=leo -db-password=12345678 -db-schema=Grpc
+./server -grpc-port=9090 -db-host=localhost:3306 -http-port=8080 -db-user=leo -db-password=12345678 -db-schema=Grpc
